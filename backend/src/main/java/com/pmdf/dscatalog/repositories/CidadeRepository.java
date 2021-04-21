653011417d55183067f4c6a358692a0c12979324
arnaldqueiroga <arnaldqueiroga@gmail.com>
2021-04-21 19:40:06 -0300

    Criado clase Cidade e Estado

---
+++ backend/src/main/java/com/pmdf/dscatalog/repositories/CidadeRepository.java
@@ -0,0 +1,32 @@
+package com.pmdf.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pmdf.dscatalog.entities.Cidade;
+
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Long>{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ backend/src/main/java/com/pmdf/dscatalog/repositories/EstadoRepository.java
@@ -0,0 +1,32 @@
+package com.pmdf.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pmdf.dscatalog.entities.Estado;
+
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long>{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
